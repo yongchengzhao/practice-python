491a3459c903b6cb4072c2eeea60860b1ea138ac
yongchengzhao <yongchengzhao@126.com>
2020-04-24 10:53:05 +0800

    - 测试 keyword。

--- crazyit/chapter01hello.py
+++ crazyit/chapter01hello.py
@@ -1,10 +1,18 @@
 # coding: utf-8
 
+import keyword
+
 
 def hello():
     print('Hello Python!')
 
 
+def test_keyword():
+    keywords = keyword.kwlist
+    print(keywords)
+    print(f'len(keywords): {len(keywords)}')
+
+
 if __name__ == '__main__':
     pass
-    hello()
+    test_keyword()
