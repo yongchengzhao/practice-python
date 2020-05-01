632475a81336b24a756b1066bfa8da43b2c21cd9
yongchengzhao <yongchengzhao@126.com>
2020-05-01 11:25:11 +0800

    增加 CSDN 的《21 天通关 Python》练习。

---
+++ crazyit/csdn-chapter01str.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+
+def test_str_repr():
+    aaa = 'aaa'
+    print(str(aaa))  # aaa
+    print(repr(aaa))  # 'aaa'
+
+
+if __name__ == '__main__':
+    print(f'----------main----------')
+    test_str_repr()
