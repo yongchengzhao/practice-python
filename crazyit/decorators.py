4f301cb71421940d3ac3fc083c139c756d570c6d
yongchengzhao <yongchengzhao@126.com>
2020-05-14 15:14:27 +0800

    基于函数的装饰器，推荐使用，虽然丧失了部分内聚性，但是更加灵活。

--- crazyit/decorators.py
+++ crazyit/decorators.py
@@ -82,3 +82,57 @@ class LogResultDecorator(BaseDecorator):
     def log_result(result):
         print(f'log_result')
         pass
+
+
+def enter_decorator(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f'Enter {func.__name__}')
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def exit_decorator(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print(f'Exit {func.__name__}')
+        return result
+
+    return wrapper
+
+
+def elapse(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(func)
+        enter_time = datetime.now()
+        print(f'Enter {func.__name__}(), the time is {enter_time}.')
+
+        result = func(*args, **kwargs)
+
+        exit_time = datetime.now()
+        print(f'Exit {func.__name__}(), the time is {exit_time}, elapse {exit_time - enter_time}.')
+
+        del enter_time, exit_time
+        return result
+
+    return wrapper
+
+
+def log_args(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f'Enter {func.__name__}(), args: {args}.')
+
+        result = func(*args, **kwargs)
+
+        print(f'Exit {func.__name__}(), result: {result}')
+        return result
+
+    return wrapper
